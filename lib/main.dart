9feb6a69835cb0c81d13ff3ba04ac0c5e87b787b
Ernest Ong YuHeng <e0031105@u.nus.edu>
2020-04-05 09:54:55 +0800

    Screen Paths, Animation, Refactoring
    Refactoring:
    Login/Registration Text Boxes
    Log In and Register Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,28 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'screens/welcome_screen.dart';
+
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
-        ),
-      ),
-      home: WelcomeScreen(),
+      // Starting Screen
+      // string is not recommended as typo 
+      // will result in app crashing
+      // initialRoute: 'welcome_screen',
+
+      // Any typos with class is much easier to spot
+      initialRoute: WelcomeScreen.id,
+
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
     );
   }
 }
